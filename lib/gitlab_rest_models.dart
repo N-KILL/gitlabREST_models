5792496dd87123a1c8b8758c3a8a1efaff283e74
NonKill <club_dog2@hotmail.com>
2023-11-29 11:36:23 -0300

    export the models

--- lib/gitlab_rest_models.dart
+++ lib/gitlab_rest_models.dart
@@ -3,3 +3,5 @@ library gitlab_rest_models;
 
 export 'src/gitlab_rest_models.dart';
 export 'src/helpers/gitlab_mapper.dart';
+export 'src/models/gitlab_payload.dart';
+export 'src/models/issue/gitlab_issue.dart';

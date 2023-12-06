d398c7fad2ace32d6f12ec15877a07648376e9b2
NonKill <club_dog2@hotmail.com>
2023-12-06 09:11:47 -0300

    Export API models

--- lib/gitlab_rest_models.dart
+++ lib/gitlab_rest_models.dart
@@ -11,5 +11,8 @@ export 'src/gitlab_endpoints/issues/issue_read.dart';
 export 'src/gitlab_endpoints/merge_requests/merge_request_create.dart';
 export 'src/gitlab_rest_models.dart';
 export 'src/helpers/gitlab_mapper.dart';
+export 'src/models/API/issues/request/issues_req_body.dart';
+export 'src/models/API/issues/response/issues_res_body.dart';
+export 'src/models/API/merge_request/merge_request_req.dart';
 export 'src/models/gitlab_payload.dart';
 export 'src/models/issue/gitlab_issue.dart';
